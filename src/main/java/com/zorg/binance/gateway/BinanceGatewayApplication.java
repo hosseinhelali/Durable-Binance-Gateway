29d23f10b7da906e195b6d469192199199a61261
hossein <hosseinhelali@gmail.com>
2021-08-17 14:48:11 +0430

    spring boot initialized project

---
+++ src/main/java/com/zorg/binance/gateway/BinanceGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.zorg.binance.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinanceGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinanceGatewayApplication.class, args);
+	}
+
+}
